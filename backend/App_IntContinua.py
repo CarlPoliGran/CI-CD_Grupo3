6ec7a0856fab72f71259cc20680e06ad3f243b3e
CarlPoliGran <carlpego@hotmail.com>
2024-05-21 23:22:45 -0500

    Backend comments and Dockerfile creation for frontend

--- backend/App_IntContinua.py
+++ backend/App_IntContinua.py
@@ -1,9 +1,11 @@
+#Importar dependencias
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
 from bson import ObjectId  
 
 app = Flask(__name__)
+#implementar CORS para los request de otros dominios
 CORS(app)
 
 # Credenciales y datos de conexión
@@ -13,12 +15,13 @@ mongo_user = 'carlos'
 mongo_password = '123'  
 mongo_db = 'dbfacturacion'
 
-# URI conexión
+# Construir la URI de conexión con las credenciales
 mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/{mongo_db}?authSource=admin"
 
 # Conexión a la base de datos Mongo
 client = MongoClient(mongo_uri)
 db = client[mongo_db]
+#creacion de la colección de usuarios
 collection = db.usuarios_db
 
 #metodo prueba creación manual docker
@@ -29,11 +32,12 @@ def prueba():
 # crear un usuario
 @app.route('/create_user', methods=['POST'])
 def create_user():
+                #json de la solicitud
     user_data = request.json
     # Verificar los campos
     if 'nombre' not in user_data or 'apellido' not in user_data or 'telefono' not in user_data:
         return 'Faltan campos requeridos (nombre, apellido, telefono)\n', 400 
-    # Insertar
+    # Insertar usuario en la coleccion
     collection.insert_one(user_data)
     return 'usuario creado correctamente\n'
 
@@ -41,11 +45,16 @@ def create_user():
 # obtener todas las facturas
 @app.route('/get_users', methods=['GET'])
 def get_users():
+    #arreglo para los usuarios de la colección
     users = []
     for user in collection.find():
+        #convertir el id en un string
         user['_id'] = str(user['_id'])
+        #agregar el usuario al arreglo
         users.append(user)
+        #retorna un json con el arreglo de usuarios
     return jsonify(users)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
